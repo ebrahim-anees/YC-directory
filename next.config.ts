47fe3b64a87562099c25a4849eb24391cbb8bc4d
Ebrahim-Anees <ebrahimaneesabuel7@gmail.com>
2025-02-05 20:21:59 +0200

    fix module compatibility

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import { withSentryConfig } from '@sentry/nextjs';
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  experimental: {
+    esmExternals: 'loose',
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
